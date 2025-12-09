1538935d91ac7ca0d3bb2d905bbe299a7b896be5
RizalMifta89 <139198094+RizalMifta89@users.noreply.github.com>
2025-12-09 22:51:35 +0700

    Create index.js

---
+++ backend/modules/tiktok/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    process: async (url) => {
+        return {
+            status: "success",
+            title: "Video TikTok dari Android",
+            download_url: "https://www.w3schools.com/html/mov_bbb.mp4",
+            original_url: url
+        };
+    }
+};
